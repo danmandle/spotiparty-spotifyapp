960ba44395af40b2473cdd7fcbd2fdb019ecb0ab
Brian Shaffer <brianshaffer@Macbook-2.local>
2013-12-10 17:49:11 -0700

    Loaded initial kitchen sink framework

--- README.md
+++ README.md
@@ -1,38 +1,74 @@
-boilerplate-app
-===============
+# Spotify Kitchen Sink
 
-This is a boilerplate app that contains the minimum required code to create
-a Spotify app. It also contains a few simple examples of how to use some of
-our APIs.
+Spotify Kitchen Sink is a demo application for the Spotify Apps API. It demonstrates some of the available functionality and is designed to help developers who are getting started. Less time reinventing the wheel, more time reinventing the record player.
 
-To test the application your Spotify account needs to have the developer flag
-set. You can find out how to activate the developer flag [here](https://developer.spotify.com/technologies/apps/#developer-account).
+## Functionality
 
-Clone this repository in your Spotify workspace directory. If you haven't got a Spotify directory, read about where to create it [here](http://developer.spotify.com/technologies/apps/guidelines/developer/#creatinganapp). Cloning will create a subfolder called boilerplate-app. 
+* Implement Tabs
+* Handle URI Arguments
+* Process dropped items (track, user, etc)
+* Play single items (track, artist, etc)
+* Play in context (album, playlist, etc)
+* Control Playback (pause, skip, etc)
+* Listen for track changes
+* Get tracks from the user’s library
+* Share a track using the built-in popup
+* Search Spotify and Local tracks
+* Create and save temporary playlists
+* Output list and album views
+* Use advanced search queries
+* Fetch metadata in a variety of different methods
+* Top Tracks for users
+* Drag n' Drop
+* Album art and overlayed player controls (when playing in a context)
 
-You can access the application (restart the Spotify desktop client if you have not done so) by typing in spotify:app:boilerplate-app in the search field.
-Read more about this in the [Spotify Developer Guidelines](http://developer.spotify.com/technologies/apps/guidelines/developer/).
 
-The **[manifest.json](https://github.com/spotify/boilerplate-app/blob/master/manifest.json)** file describes the application. For this app we have chosen
-to support all languages available.
+## Background
 
-**[index.html](https://github.com/spotify/boilerplate-app/blob/master/index.html)** loads the **[scripts/main.js]((https://github.com/spotify/boilerplate-app/blob/master/scripts/main.js)** file, our entry point to develop applications. In turn, main.js loads  four script files, each showcasing a part of the Spotify Apps API.
+The Spotify Apps API is fairly new (as of December 2011), so there’s still a lot of guesswork going on. I was able to a figure few things out, thanks to the Sample Code and a healthy dose of trial-and-error. Exploring the source code of working applications seemed to be the best way to learn, so when a friend needed help, I whipped together this Kitchen Sink. I’m now putting it up on GitHub in case it helps anyone else.
 
-* **[scripts/button-example.js](https://github.com/spotify/boilerplate-app/blob/master/scripts/button-example.js)** shows how to create some of the buttons our framework
-supports. Find out more about buttons in the [Views Framework documentation](https://developer.spotify.com/technologies/apps/docs/preview/views/buttons.html).
+If you have any questions, or would like to contribute, please get in touch. 
 
-* **[scripts/cover-example.js](https://github.com/spotify/boilerplate-app/blob/master/scripts/cover-example.js)** shows how to create a playable cover image for an album.
-Find out more about images in the [Views Framework documentation](https://developer.spotify.com/technologies/apps/docs/preview/views/image-image.html).
 
-* **[scripts/language-example.js](https://github.com/spotify/boilerplate-app/blob/master/scripts/language-example.js)** shows how to use our built-in i18n support.
-Find out more about localization in our [Spotify Developer Guidelines](http://developer.spotify.com/technologies/apps/guidelines/developer/#localization) or in our
-[API Framework documentation](http://developer.spotify.com/technologies/apps/docs/preview/api/api-core-spotifyapi-langmodule.html).
+## Resources
 
-* **[scripts/playlist-example.js](https://github.com/spotify/boilerplate-app/blob/master/scripts/playlist-example.js)** shows how to create a playlist for an album.
-Find out more about playlists in the [Views Framework documentation](https://developer.spotify.com/technologies/apps/docs/preview/views/list.html).
+Here’s a few resources that helped me out along the way:
 
-Helpful links:
+Docs
 
--  [Spotify Developer site](https://developer.spotify.com/)
--  [Spotify Developer Guidelines](http://developer.spotify.com/technologies/apps/guidelines/developer/)
--  [Spotify Apps API](https://developer.spotify.com/technologies/apps/)
+* Spotify Apps Docs: http://developer.spotify.com/en/spotify-apps-api/overview/
+* Building a Spotify App: http://musicmachinery.com/2011/12/02/building-a-spotify-app/
+* PasteBin Examples: http://pastebin.com/u/MrSiir
+* StackOverflow Questions: http://stackoverflow.com/questions/tagged/spotify (also see page 2)
+
+Read / written something helpful? Let me know and I’ll include it!
+
+Apps
+
+* Tutorial: http://developer.spotify.com/download/spotify-apps-api/tutorial/
+* Mood Knobs: https://github.com/alexmic/mood-knobs
+* Spartify: https://github.com/blixt/spartify
+* SpotifyEchoNestPlaylistDemo: https://gist.github.com/1438262
+
+Building something cool with the API? Let me know and I’ll add it!
+
+
+## Disclaimers
+
+This is an unofficial application, not sponsored or endorsed by Spotify. If you wish to develop for Spotify Apps, you must sign up for their Developer Program, and abide by their rules.
+
+This is a work in progress. There’s plenty that I’ve left out and plenty that I don’t know. If you have any suggestions or would like to add something, I would love to hear from you. 
+
+This is a hack. It’s also my first open source project. So go easy!
+
+
+## Contact
+
+pwattsmail at gmail dot com // [@ptrwtts](http://twitter.com/ptrwtts)
+
+andrew at deceptacle dot com // [@andrewburgess](http://twitter.com/andrewburgess)
+
+
+## License
+
+Distributed under the MIT license. Copyright (c) 2011 Peter Watts
---
+++ js/player.js
@@ -0,0 +1,186 @@
+$(function(){
+	// Update the page when the app loads
+	nowPlaying();
+	
+	// Listen for track changes and update the page
+	player.observe(models.EVENT.CHANGE, function (event) {
+		if (event.data.curtrack == true) {
+			var track = player.track;
+			$("#play-history").append('<div>Track changed to: '+track.name+' by '+track.album.artist.name+'</div>');
+		}
+		nowPlaying();
+		
+	}); 
+	
+	$("#commands a").click(function(e){
+		switch($(this).attr('command')) {
+			case "togglePause":
+				// Check if playing and reverse it
+				player.playing = !(player.playing);
+				e.preventDefault();
+				break;
+			case "getPosition":
+				// get position of player
+				console.log(player.position);
+				// get length of current song
+				console.log(player.track.duration);
+				e.preventDefault();
+				break;
+			case "startParty":
+				// clear playlist
+				clearPlaylist(tempPlaylist);
+
+				// load in songs, would be an ajax request
+				var data = {
+					'songs': ['spotify:track:30g7tH1rfMratchnaLfgqJ', 'spotify:track:3kZC0ZmFWrEHdUCmUqlvgZ', 'spotify:track:5yc59J3MR3tVDPTOgwgRI5', 'spotify:track:2bD1AW4yqiCurGCva6r88a', 'spotify:track:3PJikMV2xGNCooQttQrAw5']
+				};
+				if (data.songs.length > 0) {
+					$.each(data.songs, function(index, value) {
+						tempPlaylist.add(value);
+						console.log("added track uri " + value);
+					});
+				}else{
+					console.log('No songs in the queue');
+				}
+
+				// display playlist in app
+				var playlistList = new views.List(tempPlaylist);
+					playlistList.node.classList.add("temporary");
+					$("#playlistContainer").html(playlistList.node);
+
+				// var list = List.forPlaylist(tempPlaylist); // add options later, like height and number of items before scroll
+				// $('#playlistContainer').html(list.node);
+				// list.init();
+
+				// play!
+				player.play("spotify:track:30g7tH1rfMratchnaLfgqJ", tempPlaylist.data.uri, 0);
+
+				e.preventDefault();
+				break;
+			case "skip":
+				// skip to next track
+				player.next();
+				e.preventDefault();
+				break;				
+			case "playTrackFromUri":
+				// Grab a random track from your library (cause it's more fun)
+				var tracks = library.tracks;
+				var track = tracks[Math.floor(Math.random()*tracks.length)]
+				clearPlaylist(tempPlaylist);
+				tempPlaylist.add(track.data.uri);
+				player.play(track.data.uri, tempPlaylist.data.uri, 0);
+				e.preventDefault();
+				break;
+			case "playTrackFromContext":
+				// Play an item (artist, album, playlist) from a particular position
+				player.play(
+					$(this).attr('href'),				// Item to play
+					$(this).attr('href'),				// Context to use
+					parseInt($(this).attr('pos'))		// Position to play from
+				);
+				e.preventDefault();
+				break;
+			case "showSharePopup":
+				// skip to next track
+				application.showSharePopup(document.getElementById($(this).attr('id')),player.track.uri); // This will fail if you're listening to a local track :(
+				e.preventDefault();
+				break;
+		}
+	});
+	
+});
+
+function clearPlaylist(playlist) {
+	while (playlist.data.length > 0) {
+		playlist.data.remove(0);
+	}
+}
+
+function updateLocalPlaylist() {
+	models.Playlist.createTemporary('Spotiparty').done(function(p) {
+		console.log("playlist " + p.name + " created");
+		p.load("tracks").done(function(spotipartyPlaylist) {
+			window.spotipartyPlaylist = spotipartyPlaylist;
+
+			spotipartyPlaylist.tracks.clear();
+			updatePlaylistFromWeb(spotipartyPlaylist);
+
+			setTimeout($.proxy(function() {
+				var list = List.forPlaylist(p); // add options later, like height and number of items before scroll
+				$('#playlistContainer').html(list.node);
+				list.init();
+			}, this), 10);
+		});
+	}).fail(function(error) {
+		console.log(error);
+	});
+}
+
+function updatePlaylistFromWeb(localPlaylist) {
+	// AJAX call would go here...
+
+	var data = {
+		'songs': ['spotify:track:30g7tH1rfMratchnaLfgqJ', 'spotify:track:3kZC0ZmFWrEHdUCmUqlvgZ', 'spotify:track:5yc59J3MR3tVDPTOgwgRI5', 'spotify:track:2bD1AW4yqiCurGCva6r88a', 'spotify:track:3PJikMV2xGNCooQttQrAw5']
+	};
+	// var data = {'songs':[]};
+
+	if (data.songs.length > 0) {
+
+		// localPlaylist.tracks.insert(0, data.songs); // that 0 might not be right
+
+		$.each(data.songs, function(index, value) {
+			localPlaylist.tracks.add(models.Track.fromURI(value));
+
+			console.log("added track uri " + value);
+		});
+	} else {
+		console.log('No songs in the queue');
+	}
+}
+
+function nowPlaying() {
+
+	// This will be null if nothing is playing.
+	var track = player.track;
+
+	if (track == null) {
+		$("#now-playing").html("Painful silence!");
+	} else {
+		var link = null;
+		if (player.context)
+			link = new models.Link(player.context);
+		else
+			link = new models.Link(player.track.uri);
+			
+		if (link.type === models.Link.TYPE.ARTIST)
+			playerImage.context = models.Artist.fromURI(link.uri);
+		else if (link.type === models.Link.TYPE.PLAYLIST)
+			playerImage.context = models.Playlist.fromURI(link.uri);
+		else if (link.type === models.Link.TYPE.INTERNAL) {
+			if (tempPlaylist.length > 0)
+				playerImage.context = tempPlaylist;
+		}
+			
+		$("#now-playing").empty();
+		var cover = $(document.createElement('div')).attr('id', 'player-image');
+
+		if (link.type === models.Link.TYPE.TRACK || link.type === models.Link.TYPE.LOCAL_TRACK ||
+			(link.type === models.Link.TYPE.INTERNAL && tempPlaylist.length == 0)) {
+			cover.append($(document.createElement('a')).attr('href', track.data.album.uri));
+			var img = new ui.SPImage(track.data.album.cover ? track.data.album.cover : "sp://import/img/placeholders/300-album.png");
+			cover.children().append(img.node);
+		} else {
+			cover.append($(playerImage.node));
+		}
+		
+		$("#now-playing").append(cover);
+		
+		var song = '<a href="'+track.uri+'">'+track.name+'</a>';
+		var album = '<a href="'+track.album.uri+'">'+track.album.name+'</a>';
+		var artist = '<a href="'+track.album.artist.uri+'">'+track.album.artist.name+'</a>';
+		var context = player.context, extra ="";
+		if(context) { extra = ' from <a href="'+context+'">here</a>'; } // too lazy to fetch the actual context name
+		$("#now-playing").append(song+" by "+artist+" off "+album+extra);
+	}
+	
+}
