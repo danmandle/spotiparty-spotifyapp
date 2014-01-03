e0158e497e3e43b2bff55adb9a99175db6aa4eea
Shane McDermed <shane.mcdermed@tapinfluence.com>
2014-01-03 12:00:32 -0700

    Stuff happened

--- scripts/party-config.js
+++ scripts/party-config.js
@@ -1,27 +1,27 @@
 require(['$api/models', '$api/location'], function(models, location) {
-  window.socket = io.connect('http://127.0.0.1:1337');
-  socket.on('connect', function() {
-    alert('connect')
-  });
-  var Party = {};
-  var user = models.User.fromURI('spotify:user:@');
-    user.load('username', 'name').done(function(u) {
-      $('#user').val(u.identifier);
-      Party.user = u;
-  });
-  var loc = location.Location.query();
-  loc.load(['latitude']).done(function(loc) {
-    console.log("Latitude: " + loc.latitude);
-  });
+  // window.socket = io.connect('http://127.0.0.1:1337');
+  // socket.on('connect', function() {
+  //   alert('connect')
+  // });
+  // var Party = {};
+  // var user = models.User.fromURI('spotify:user:@');
+  //   user.load('username', 'name').done(function(u) {
+  //     $('#user').val(u.identifier);
+  //     Party.user = u;
+  // });
+  // var loc = location.Location.query();
+  // loc.load(['latitude']).done(function(loc) {
+  //   console.log("Latitude: " + loc.latitude);
+  // });
 
   $('#createPartyForm').on('submit', function(evt) {
     evt.preventDefault();
     var $form = $(this);
     var name = $form.find('#partyName').val();
     var user = $form.find('#user').val();
-    
+
     $.ajax({
-		url: 'http://192.168.10.152:1337/party',
+		url: 'http://127.0.0.1:1337/party',
 		type: 'POST',
 		crossDomain: true,
 		data: {
